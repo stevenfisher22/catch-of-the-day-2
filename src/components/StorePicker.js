e38af52dbdfbd7235b6406b351f65a2f7073037e
Steven Fisher <steven.fisher22@gmail.com>
2019-02-21 17:59:05 -0600

    setup

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+
+    render() {
+        return (
+            <p>This is the Store Picker.</p>
+        );
+    }
+}
+
+export default StorePicker
