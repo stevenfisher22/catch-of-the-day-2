a1182314a6221e2c6192079f37c0245cc70638e1
Steven Fisher <steven.fisher22@gmail.com>
2019-02-22 16:25:21 -0600

    Fish.js and State

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
     state = {
@@ -19,6 +21,12 @@ class App extends React.Component {
             // fishes = fishes 
             fishes
         })
+    };
+
+    loadSampleFishes = () => {
+        this.setState({
+            fishes: sampleFishes
+        })
     }
 
     render() {
@@ -26,9 +34,13 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} 
+                        details={this.state.fishes[key]} />)}
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
             </div>
         );
     }
