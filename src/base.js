36a5f22a8f5db0887939757c2fc1e394249d1cff
Steven Fisher <steven.fisher22@gmail.com>
2019-02-24 17:44:20 -0600

    updated

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-        apiKey: "AIzaSyD2jmIoRV48KD0ztH-wKZHkVqNNLV81me0",
+        apiKey: "",
         authDomain: "catch-of-the-day-5f0b6.firebaseapp.com",
         databaseURL: "https://catch-of-the-day-5f0b6.firebaseio.com",
 });
